e7a97fa03a208e486c6fcbc14bd59eef59e0a1e7
Vlad <koroliuk.work@gmail.com>
2022-02-03 23:27:00 +0200

    fix naming

--- routes/comment.js
+++ routes/comment.js
@@ -5,12 +5,12 @@ import comment from "../controllers/comment.js";
 const router = new Router();
 
 // GET
-router.get("/comments", auth, comment.get);
+router.get("/", auth, comment.get);
 // POST
-router.post("/comment", auth, comment.create);
+router.post("/", auth, comment.create);
 // PATCH
-router.patch("/comment", auth, comment.change);
+router.patch("/", auth, comment.change);
 // DELETE
-router.delete("/comment", auth, comment.delete);
+router.delete("/", auth, comment.delete);
 
 export default router;
